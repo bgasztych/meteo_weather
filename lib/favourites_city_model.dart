182a0fcd5ae77eb15debfeaffd8f40c328648a12
Bartosz Gasztych <bartosz.gasztych@gmail.com>
2020-10-13 19:54:46 +0200

    add search delegate

--- lib/favourites_city_model.dart
+++ lib/favourites_city_model.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:meteo_weather/logger.dart';
 
 class FavouriteCityModel extends ChangeNotifier {
   static List<City> cities = [
@@ -8,23 +9,37 @@ class FavouriteCityModel extends ChangeNotifier {
     City(3, "Poznań", "wielkopolskie", null),
     City(4, "Gdańsk", "pomorskie", null),
     City(5, "Jelenia Góra", "dolnoślaskie", null),
+    City(6, "Wronki", "dolnoślaskie", null),
   ];
 
   List<City> _favouritesCities = [];
 
   void add(City city) {
-    _favouritesCities.add(city);
-    notifyListeners();
+    if (!_favouritesCities.contains(city)) {
+      _favouritesCities.add(city);
+      notifyListeners();
+    }
   }
 
   void remove(City city) {
     _favouritesCities.remove(city);
+    notifyListeners();
   }
 
   City get(int position) => _favouritesCities[position];
 
   int length() => _favouritesCities.length;
 
+  List<City> searchCities(String query) {
+    List<City> matchCities = [];
+    cities.forEach((element) {
+      if (element.city.toLowerCase().startsWith(query.toLowerCase())) {
+        Logger.d(element);
+        matchCities.add(element);
+      }
+    });
+    return matchCities;
+  }
 }
 
 @immutable
@@ -39,8 +54,20 @@ class City {
   @override
   bool operator ==(Object other) =>
       identical(this, other) ||
-      other is City && runtimeType == other.runtimeType && id == other.id;
+          other is City && runtimeType == other.runtimeType && id == other.id;
 
   @override
   int get hashCode => id.hashCode;
+
+  @override
+  String toString() {
+    return 'City{id: $id, city: $city, voivodeship: $voivodeship, imageUri: $imageUri}';
+  }
+
+  Map toJson() => {
+    'id': id,
+    'city': city,
+    'voivodeship': voivodeship,
+    'imageUri': imageUri
+  };
 }
--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -14,30 +14,101 @@ class HomeScreen extends StatelessWidget {
           title: Text("Favourites"),
           actions: [
             IconButton(
-                icon: Icon(Icons.search),
-                onPressed: () => favouriteCityModel.add(
-                    FavouriteCityModel.cities[
-                        Random().nextInt(FavouriteCityModel.cities.length)])),
+              icon: Icon(Icons.search),
+              onPressed: () => showSearch(
+                context: context,
+                delegate: CitySearchDelegate(favouriteCityModel),
+              ),
+            ),
           ],
         ),
         body: ListView.builder(
           itemCount: favouriteCityModel.length(),
-          itemBuilder: (BuildContext context, int position) =>
-              _buildItem(favouriteCityModel.get(position)),
+          itemBuilder: (BuildContext context, int position) => _buildItem(
+              context, favouriteCityModel, favouriteCityModel.get(position)),
         ),
       ),
     );
   }
 
-  Widget _buildItem(City city) {
+  Widget _buildItem(BuildContext context, FavouriteCityModel model, City city) {
     return Card(
       child: Row(
         children: [
           SizedBox(width: 8),
           Expanded(child: Text(city.city)),
-          IconButton(icon: Icon(Icons.star), onPressed: null)
+          IconButton(
+              icon: Icon(Icons.delete),
+              onPressed: () => _onRemoveCityPressed(context, model, city))
         ],
       ),
     );
+  }
+
+  void _onRemoveCityPressed(
+      BuildContext context, FavouriteCityModel model, City city) {
+    showDialog(
+        context: context,
+        barrierDismissible: false,
+        builder: (context) => AlertDialog(
+              title: Text("Remove from favourites?"),
+              actions: [
+                FlatButton(
+                    onPressed: () => Navigator.pop(context),
+                    child: Text("Cancel")),
+                FlatButton(
+                    onPressed: () {
+                      model.remove(city);
+                      Navigator.pop(context);
+                    },
+                    child: Text("Remove"))
+              ],
+            ));
+  }
+}
+
+class CitySearchDelegate extends SearchDelegate {
+  FavouriteCityModel _model;
+
+  CitySearchDelegate(this._model);
+
+  @override
+  List<Widget> buildActions(BuildContext context) {
+    return [IconButton(icon: Icon(Icons.clear), onPressed: () => query = "")];
+  }
+
+  @override
+  Widget buildLeading(BuildContext context) {
+    return IconButton(
+        icon: Icon(Icons.arrow_back), onPressed: () => close(context, null));
+  }
+
+  @override
+  Widget buildResults(BuildContext context) {
+    if (query.length < 3) {
+      return Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Center(
+            child: Text("Search term must be longer than two letters."),
+          )
+        ],
+      );
+    }
+
+    List<City> cities = _model.searchCities(query);
+
+    return ListView.builder(
+        itemCount: cities.length,
+        itemBuilder: (context, index) {
+          return ListTile(
+            title: Text(cities[index].city),
+          );
+        });
+  }
+
+  @override
+  Widget buildSuggestions(BuildContext context) {
+    return Column();
   }
 }
