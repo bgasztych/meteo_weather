c69869c9dcfcd287a9b988c2951ea7729da634df
Bartosz Gasztych <bartosz.gasztych@gmail.com>
2020-10-19 17:22:05 +0200

    meteogram screen with legend

--- lib/meteogram_screen.dart
+++ lib/meteogram_screen.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:meteo_weather/favourites_city_model.dart';
+import 'package:photo_view/photo_view.dart';
 
 class MeteogramScreen extends StatelessWidget {
   final City city;
+
+  static const double LEGEND_PERCENT_WIDTH = 0.307692307692;
+  static const double METEOGRAM_PERCENT_WIDTH = 0.692307692308;
 
   MeteogramScreen({Key key, @required this.city}) : super(key: key);
 
@@ -10,7 +14,25 @@ class MeteogramScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text(city.city)),
-      body: Center(child: Text("placeholder")),
+      // body: PhotoView(
+      //   imageProvider: ,
+      // ),
+      body: PhotoView.customChild(
+        backgroundDecoration: BoxDecoration(color: Colors.white),
+        child: Row(
+          children: [
+            Image(
+              image: AssetImage('lib/resources/images/legend.png'),
+              width: MediaQuery.of(context).size.width * LEGEND_PERCENT_WIDTH,
+            ),
+            Image(
+              image: AssetImage('lib/resources/images/meteogram.png'),
+              width:
+                  MediaQuery.of(context).size.width * METEOGRAM_PERCENT_WIDTH,
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
