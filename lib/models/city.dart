96b6de222325527ab2c296e183e34e7beee392ed
Bartosz Gasztych <bartosz.gasztych@gmail.com>
2020-11-13 15:44:02 +0100

    update city model

--- lib/models/city.dart
+++ lib/models/city.dart
@@ -1,20 +1,30 @@
 import 'package:equatable/equatable.dart';
 import 'package:flutter/material.dart';
 
-@immutable
 class City extends Equatable {
   final int id;
   final String city;
   final String voivodeship;
-  final String imageUri;
+  String meteogramBase64;
+  DateTime updatedDate;
 
-  City(this.id, this.city, this.voivodeship, this.imageUri);
+  City(this.id, this.city, this.voivodeship, this.meteogramBase64, this.updatedDate);
 
   @override
-  List<Object> get props => [id, city, voivodeship, imageUri];
+  List<Object> get props => [id, city, voivodeship];
+
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'city': city,
+      'voivodeship': voivodeship,
+      'meteogramBase64': meteogramBase64,
+      'updatedDate': updatedDate.millisecondsSinceEpoch,
+    };
+  }
 
   @override
   String toString() {
-    return 'City{id: $id, city: $city, voivodeship: $voivodeship, imageUri: $imageUri}';
+    return 'City{id: $id, city: $city, voivodeship: $voivodeship, imageUri: $meteogramBase64, updatedDate: $updatedDate}';
   }
 }
